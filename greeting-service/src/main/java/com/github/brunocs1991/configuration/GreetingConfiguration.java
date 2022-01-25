977719112a8b428239da6cb22f2f6091e38d021d
Brunocs1991 <brunocosta.1991@hotmail.com>
2022-01-25 09:40:54 -0300

    Atualizando sem reiniciar endpoint

--- greeting-service/src/main/java/com/github/brunocs1991/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/com/github/brunocs1991/configuration/GreetingConfiguration.java
@@ -1,19 +1,21 @@
 package com.github.brunocs1991.configuration;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 @Getter
 @Setter
+@NoArgsConstructor
 public class GreetingConfiguration {
 
     private String greeting;
     private String defaultValue;
 
-    public GreetingConfiguration() {
-    }
 }
