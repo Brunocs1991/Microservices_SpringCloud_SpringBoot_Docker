fcc52569ef7b25298f6e579fda3d577a65fbc75b
Bruno <brunocosta.1991@hotmail.com>
2022-01-27 22:28:37 +0000

    Consumindo endpoint cambio service

--- 06-book-service/src/main/java/com/github/brunocs1991/service/BookService.java
+++ 06-book-service/src/main/java/com/github/brunocs1991/service/BookService.java
@@ -1,11 +1,14 @@
 package com.github.brunocs1991.service;
 
 import com.github.brunocs1991.model.Book;
+import com.github.brunocs1991.model.to.CambioTO;
 import com.github.brunocs1991.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 @Service
@@ -23,8 +26,15 @@ public class BookService {
         if(Objects.isNull(book)){
             throw new RuntimeException("Book not Found");
         }
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+        var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", CambioTO.class, params);
+        var cambio = response.getBody();
         var port = environment.getProperty("local.server.port");
         book.setEnviroment(port);
+        book.setPrice(cambio.getConvertedValue());
         return book;
     }
 }
