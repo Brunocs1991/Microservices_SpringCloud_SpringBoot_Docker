de23d2e67124635c7c15528284fda4eaff28493d
Bruno <brunocosta.1991@hotmail.com>
2022-02-03 18:04:38 -0300

    Dockerizando uma aplicação spring boot

---
+++ 09-hello-docker/src/main/java/com/github/brunocs1991/model/HelloDocker.java
@@ -0,0 +1,13 @@
+package com.github.brunocs1991.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class HelloDocker {
+    private String content;
+    private String enviroment;
+}
